e57a60a1838b9b85e2bcc23a96e982b786f9c1ad
LantosAbel <96252679+LantosAbel@users.noreply.github.com>
2022-01-06 01:41:22 +0100

    Add files via upload

---
+++ Beadandó2.R
@@ -0,0 +1,44 @@
+#2. Hozz létre egy generális fgv-t, amely listát vár bemenetként, 
+#amely listának minden eleme egy data.frame. A fgv  próbálja meg ezeket 
+#összefûzni automatikusan. Ha sikerül összeilleszteni -akár csak néhányat közölük,
+#akkor ezt végezze el, és egy attributumban adja hozzá az eredmény data.frame-hez,
+#hogy mely listaelemeket hogyan fûzött összefûzi. 
+#Majd az eredmény legyen ez a data.frame.
+
+lista1 = list(ess1,ess2,ess3,ess4)
+
+fgv = function(a){
+  if(is.list(a)){
+    a1 <- a
+  }  
+   else stop("nem lista!")  
+  
+  for (i in length(a1)){
+    if (!is.data.frame(a1[[i]])){
+      stop("egy vagy több lista elem nem data frame!")
+    }
+    else next
+  }
+  
+  egyben<<-Reduce(function(df1,df2) merge(df1,df2,all=TRUE,no.dups=TRUE),a1)
+  
+       tagok <- c("A bemeneti lista ezen tagjai lettek összemergelve:",1:length(a1))
+        attr(egyben,"tagok") <<- tagok
+}    
+  
+
+fgv(lista1)
+attr(egyben,"tagok")
+
+tagok
+
+for (j in a1[[length((a1))]]){     
+x <- data.frame(i = c("a","b","c"), j = 1:3, stringsAsFactors=FALSE)
+y <- data.frame(i = c("b","c","d"), k = 4:6, stringsAsFactors=FALSE)
+z <- data.frame(i = c("c","d","a"), l = 7:9, stringsAsFactors=FALSE)   
+teszt = list(x,y,z)
+
+
+rm(tagok)
+trick= list(ess1,v)    
+v =3 
